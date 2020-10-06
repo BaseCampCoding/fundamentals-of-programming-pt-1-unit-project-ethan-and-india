6e6ea92077711c383d4eddf888852d0741a87e2c
Ethan Bishop <ebishop21@basecampcodingacademy.org>
2020-10-06 14:58:22 -0500

    Input Name Validation Function Defined and Moved to ESSENTIAL Functions

--- ESSENTIAL_FUNCTIONS.py
+++ ESSENTIAL_FUNCTIONS.py
@@ -1,11 +1,10 @@
-students_list = [
-    ["ethan bishop", "india ward", "charles mardis", "joseph dandan", "lathe ward", "seth hogue", "jeremiah tatum", "travis alexander", "sean coh", "loveen kaur", "kenia sandoval", "lance easley", "gregory bowles", "dylan mccreary", "aj hardaway", "al dees", "asiah moton", "bryce taylor", "jeremiah bass", "evan sanders", "matthew wilson", "andy duarte"]
+students_list = ["ethan bishop", "india ward", "charles mardis", "joseph dandan", "lathe ward", "seth hogue", "jeremiah tatum", "travis alexander", "sean coh", "loveen kaur", "kenia sandoval", "lance easley", "gregory bowles", "dylan mccreary", "aj hardaway", "al dees", "asiah moton", "bryce taylor", "jeremiah bass", "evan sanders", "matthew wilson", "andy duarte"]
 
-def input_student_name(prompt: str) -> int:
-  while True:
-    response = input(prompt)
-    if response == students_list[:-1]
-      response = int(response)
-      if response >= 0:
-        return response
-    print("Please Enter a Valid Name")
+def input_name():
+    while True:
+        name = input("Name (First Last): ").lower()
+        if name in students_list:
+            print("Successfully Logged In!")
+            return name
+        else: 
+            print("Please Provide A Valid Name!")
--- students.py
+++ students.py
@@ -1,3 +1,3 @@
 import ESSENTIAL_FUNCTIONS
 print("Good Morning, Welcome to Class, Please Sign in")
-name = ESSENTIAL_FUNCTIONS.is_student_name_valid("Name (First, Last): ")  
+x = ESSENTIAL_FUNCTIONS.input_name()
