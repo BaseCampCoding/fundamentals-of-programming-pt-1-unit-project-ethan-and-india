c0b9db3e5af62c602a267d3630c6b73cb5453be4
Ethan Bishop <ebishop21@basecampcodingacademy.org>
2020-10-06 15:58:20 -0500

    Small Grammatical Changes, Plust a time test

--- students.py
+++ students.py
@@ -1,3 +1,8 @@
 import ESSENTIAL_FUNCTIONS
-print("Good Morning, Welcome to Class, Please Sign in")
+import time
+print("Good Morning, Welcome to Class! Please Sign in!")
 x = ESSENTIAL_FUNCTIONS.input_name()
+print('The time is currently  :', time.ctime())
+clock_in = input("Are you ready to clock in. If so type (clock-in): ")
+    
+
