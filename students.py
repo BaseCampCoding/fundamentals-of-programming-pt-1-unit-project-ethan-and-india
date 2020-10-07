ba750693521e0fb92b5868e8758d24ab36a53f30
Ethan Bishop <38193769+ecbishop1055@users.noreply.github.com>
2020-10-07 14:44:55 -0500

    Clock in/ Input Validation

--- students.py
+++ students.py
@@ -2,7 +2,14 @@ import ESSENTIAL_FUNCTIONS
 import time
 print("Good Morning, Welcome to Class! Please Sign in!")
 x = ESSENTIAL_FUNCTIONS.input_name()
-print('The time is currently  :', time.ctime())
-clock_in = input("Are you ready to clock in. If so type (clock-in): ")
+while True:
+    clock_in = input("Are you ready to clock in. If so type (clock-in): ")
+    if clock_in == "clock-in":
+        break
+    else:
+        print("Type clock-in please!")
+print('You have clocked in   :', time.ctime())
+your_data = {"Purchase Amount": 'TotalAmount'}
+print(your_data,  file=open('D:\LOGINTIMES', 'w'))
     
 
