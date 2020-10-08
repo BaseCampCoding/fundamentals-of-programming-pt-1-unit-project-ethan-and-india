b969cbe1c09c78d02600b6efc5d2af21c5976c98
India-Ward <72226272+India-Ward@users.noreply.github.com>
2020-10-08 10:13:19 -0500

    I converted the times from import time to import datetime, and I also converted datetime into string.

--- students.py
+++ students.py
@@ -1,6 +1,8 @@
 def main():
     import ESSENTIAL_FUNCTIONS
-    import time
+    from datetime import datetime
+    dateTimeObj = datetime.now()
+    timestampStr = dateTimeObj.strftime("%d-%b-%Y (%H:%M:%S.%f)") 
     print("Good Morning, Welcome to Class! Please Sign in!")
     x = ESSENTIAL_FUNCTIONS.input_name()
     while True:
@@ -9,14 +11,14 @@ def main():
             break
         else:
             print("Type clock-in please!")
-    print('You have clocked in   :', time.ctime())
+    print('You have clocked in   :',  timestampStr )
     your_data = {"Purchase Amount": 'TotalAmount'}
     with open('LOGINTIMES.txt', 'a') as f:
         f.write('\n')
         f.write(x ) 
         f.write('\n')
         f.write('\n Clocked In at ') 
-        f.write(time.ctime())
+        f.write( timestampStr)
         f.write('\n')
     restart = input("Type next to allow the next student to clock in: ").lower()
     if restart == "next":
